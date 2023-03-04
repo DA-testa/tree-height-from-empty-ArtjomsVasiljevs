c4738379806abaa09653f8b26d773a4a2ad03aa6
ArtjomsVasiljevs <118185097+ArtjomsVasiljevs@users.noreply.github.com>
2023-03-04 23:19:37 +0000

    try7

--- tree_height.py
+++ tree_height.py
@@ -2,19 +2,27 @@
 
 import sys
 import threading
+import numpy
 
+class nodes:
+    def __init__(self, parent, child=None):
+        self.parent = parent
+        self.child = child
 
-def compute_height(n, parents):
+    def add(self, node):
+        if self.child is None:
+            self.child = []
+        self.child.append(node)
+
+def compute_height(node):
     # Write this function
-    max_height = 0
-    for v in range(n):
-        height = 0
-        cur = v 
-        while cur != -1:
-            height += 1
-            cur = parents[cur]
-        max_height = max(max_height, height)
-    return max_height
+    if node.child is None:
+        return 0
+    child1 = node.child
+    heightlist = []
+    for child in child1:
+        heightlist.append(compute_height(child))
+    return max(heightlist, default=0)+1
 
 def main():
     cmd = input()
@@ -29,11 +37,42 @@ def main():
             n = int(sep[0])
             parents_1 = sep[2].split(" ")
             parents = ([int(x) for x in parents_1])
-            print (compute_height(n, parents))
+            node_l = []
+            for i in range(n):
+                node_l.append(nodes(parents[i]))
+            for child_index in range(n):
+                parent_index = parents[child_index]
+                if parent_index == -1:
+                    root = child_index
+                else:
+                    node_l[parent_index].add(node_l[child_index])
+
+            if len(node_l) == 0:
+                return 0
+    
+            height = compute_height(node_l[root]) + 1
+    
+            print(height)
+
     elif "I" in cmd:
         n = int(input())
         parents = list(map(int, input().split()))
-        print(compute_height(n, parents))
+        node_l = []
+        for i in range(n):
+                node_l.append(nodes(parents[i]))
+        for child_index in range(n):
+            parent_index = parents[child_index]
+            if parent_index == -1:
+                root = child_index
+            else:
+                node_l[parent_index].add(node_l[child_index])
+
+        if len(node_l) == 0:
+            return 0
+    
+        height = compute_height(node_l[root]) + 1
+    
+        print(height)
 
     # implement input form keyboard and from files
     
@@ -48,7 +87,7 @@ def main():
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
 # of bigger stack, we have to launch the computation in a new thread.
-#sys.setrecursionlimit(10**7)  # max depth of recursion
-#threading.stack_size(2**27)   # new thread will get stack of such size
-#threading.Thread(target=main).start()
+sys.setrecursionlimit(10**7)  # max depth of recursion
+threading.stack_size(2**27)   # new thread will get stack of such size
+threading.Thread(target=main).start()
 main()
